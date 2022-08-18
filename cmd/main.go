c8aec23e6667381e4764b8b5243f5f5d76dbe2bc
Sergey Baraulia <baraulia@yandex.ru>
2022-08-18 21:31:52 +0300

    Added postgres, logrus

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,32 @@
-package cmd
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"itec.chat/pkg/logging"
+	"itec.chat/pkg/repositories/postgres"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file. %s", err.Error())
+	}
+
+	logger := logging.GetLogger()
+
+	db, err := postgres.NewPostgresDB(&postgres.PostgresDB{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		Username: os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		DBName:   os.Getenv("POSTGRES_DB"),
+		SSLMode:  os.Getenv("POSTGRES_SSL_MODE"),
+		Logger:   logger,
+	})
+	if err != nil {
+		logger.Panicf("Error while initialisation database:%s", err)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module itec.chat
 
 go 1.18
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.6
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
