06e503e8d8010275ed6ac947970b3d17459e9809
visbm <visbm@mail.ru>
2022-09-05 14:27:34 +0300

    added user service logic

--- internal/service/userService.go
+++ internal/service/userService.go
@@ -19,24 +19,52 @@ func NewUserService(repo *repository.Repository, logger logging.Logger) UserServ
 }
 
 func (u *userService) GetByID(id int) (user *models.User, err error) {
-	return nil, nil
+	user, err = u.repo.User.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 func (u *userService) GetAll(limit, offset int) (users []models.User, err error) {
-	return nil, nil
+	users, err = u.repo.User.GetAll(limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 func (u *userService) Create(createUser *models.CreateUser) (id int, err error) {
-	return 0, nil
+	id, err = u.repo.User.Create(createUser)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
 func (u *userService) Update(updateUser *models.UpdateUser) (err error) {
+	err = u.repo.User.Update(updateUser)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 func (u *userService) Delete(id int) (err error) {
+	err = u.repo.User.Delete(id)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (u *userService) GetUsersByChatID(id int) (users []models.User, err error) {
-	return nil, nil
+	users, err = u.repo.User.GetUsersByChatID(id)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 func (u *userService) UpdateStatus(updateUserStatus *models.UpdateUserStatus) (err error) {
+	err = u.repo.User.UpdateStatus(updateUserStatus)
+	if err != nil {
+		return err
+	}
 	return nil
 }
